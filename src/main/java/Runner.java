7a5a9b8924f9d4d5105537a894a76548ef6a3329
nermin okuyucu <nerminmercan.45@gmail.com>
2024-10-03 23:34:37 +0300

    tech3

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,5 +6,6 @@ public class Runner {
         System.out.println("versiyon 3");
         System.out.println("techpro 1");
         System.out.println("techpro 2");
+        System.out.println("techpro 3");
     }
 }
