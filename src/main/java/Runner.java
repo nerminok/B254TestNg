174ef9b1eb85090b6cc8bc61f302e8534347b7fa
nerminmercan <nerminmercan.45@gmail.com>
2024-10-04 23:27:46 +0300

    github1

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,4 +1,5 @@
 public class Runner {
+    
     public static void main(String[] args) {
 
         System.out.println("versiyon 1");
@@ -8,5 +9,8 @@ public class Runner {
         System.out.println("techpro 2");
         System.out.println("techpro 3");
         //remotada degisiklik
+        //githubta commit
+
+        
     }
 }
